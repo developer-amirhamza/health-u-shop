8854681a10172042ee8bbbbdff321fd09bd993b1
Amir Hamza <amirhamza27940@gmail.com>
2026-03-12 13:20:04 +0600

    larabel

--- client/src/app/components/Header.tsx
+++ client/src/app/components/Header.tsx
@@ -2,37 +2,52 @@ import Image from 'next/image'
 import React from 'react'
 import { IoCall } from 'react-icons/io5'
 import logo from "@/assets/logo-img.png"
+import { IoMdCart } from 'react-icons/io'
+import { TiShoppingCart } from 'react-icons/ti'
+import { FaSearch } from 'react-icons/fa'
 
 const Header = () => {
-  return (
-    <div className="">
-        {/* top bar */}
-        <div className=" w-full max-h-10 bg-primary text-foreground flex justify-center items-center ">
-            <div className="container flex items-center justify-end w-full">
-                <div className="flex w-2/6"></div>
-                <div className="flex justify-between w-full text-white font-medium items-center">
-                    <p className="text-lg  text-end"> Looking for something specific? Click here! </p>
-                    <div className="flex items-center gap-3 font-medium ">
-                        <IoCall  />
-                        <a className='font-medium text-lg ' href="tel:+0481707758">0481 707 758</a>
+    return (
+        <div className="">
+            {/* top bar */}
+            <div className=" w-full max-h-10 bg-primary text-foreground flex py-1 justify-center items-center ">
+                <div className="container flex items-center justify-end w-full">
+                    <div className="flex w-2/6"></div>
+                    <div className="flex justify-between w-full text-white font-medium items-center">
+                        <p className="text-lg  text-end"> Looking for something specific? Click here! </p>
+                        <div className="flex items-center gap-3 font-medium ">
+                            <IoCall />
+                            <a className='font-medium text-lg ' href="tel:+0481707758">0481 707 758</a>
+                        </div>
                     </div>
                 </div>
             </div>
+            {/* nav bar */}
+            <div className="flex w-full bg-neutral-200 h-full items-center">
+                {/* logo */}
+                <div className="container flex items-center justify-between w-full">
+                    <div className="flex py-3 w-full">
+                        <Image src={logo} className='object-scale-down h-14 ' alt='Health U Shop' />
+                    </div>
+                    {/* search bar */}
+                    <div className="w-full min-w-2xl  flex items-center justify-between  ">
+                        <input type="text" className="outline-none flex border w-full  border-neutral-400 focus-within:border-secondary py-2.5 px-5 rounded-md text-lg " placeholder='' />
+                        <span className="text-neutral-500 w-full flex absolute justify-self-center ml-20 font-medium ">Search Products Like: "T-Shirt"</span>
+                        <FaSearch  className='absolute ml-4 text-2xl text-neutral-400 '/>
+                    </div>
+                    {/* left side */}
+                    <div className="w-full flex gap-4 items-center justify-end  ">
+                        <span className="text-xl font-medium text-neutral-800 cursor-pointer hover:text-secondary transition-colors duration-300 ">Login</span>
+                        <div className="flex gap-2 bg-primary items-center justify-between py-2.5 px-4 rounded-md text-white ">
+                            <TiShoppingCart size={28} className=' animate-bounce ' />
+                            <p className="font-semibold  ">My Cart</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        {/* nav bar */}
-        <div className="flex w-full bg-neutral-200 h-full items-center">
-            {/* logo */}
-            <div className="container flex items-center justify-between ">
-                <div className="flex py-3">
-                <Image src={logo} className='object-scale-down h-14 ' alt='Health U Shop' />
-            </div>
-            <div className="">
-                <input type="text" className="outline-none border border-secondary py-2.5 px-5 rounded-md text-lg " placeholder='search products' />
-            </div>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
---
+++ client/src/app/components/Search.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import { FaArrowLeft, FaSearch } from "react-icons/fa";
+import Link from 'next/link';
+
+
+const Search = () => {
+    // const navigate = useNavigate();
+    // const location = useLocation()
+    const [isSearchPage, setIsSearchPage] = useState(false);
+    // const isMobile = useMobile();
+    // const params = useLocation();
+    const searchText = params?.search?.slice(7);
+
+    const handleOnChange = (e:any)=>{
+        const value = e.target.value;
+        const url = `/search?query=${value}`
+        // navigate(url)
+    }
+    useEffect(() => {
+        const isSearch = location.pathname === "/search"
+        setIsSearchPage(isSearch)
+    }, [location])
+    return (
+        <Link onClick={() => navigate("/search")} className='flex w-full border gap-2 group lg:min-w-[420px] bg-slate-100 focus-within:bg-amber-50 h-11 items-center
+         min-w-75 rounded-md border-neutral-300  focus-within:border-primary'>
+
+            {isSearchPage && isMobile ?
+                (
+                    <Link href={'/'}
+                    onClick={(e)=> e.stopPropagation()}
+                    className=' p-2 rounded-full m-1 shadow-md text-neutral-500 bg-white group-focus-within:text-primary'>
+                        <FaArrowLeft size={22} />
+                    </Link>
+                ) : (
+                    <button className=' p-3 text-neutral-500 group-focus-within:text-primary'>
+                        <FaSearch size={22} />
+                    </button>
+                )
+            }
+            {!isSearchPage ? (
+                <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'Search "Oil Filter"',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'Search "Brakes"',
+                        1000,
+                        'Search "Horns"',
+                        1000,
+                        'Search "Spark Plugs"',
+                        1000,
+                        'Search "Wiper blade"',
+                        1000,
+                        'Search "Additive"',
+                        1000,
+                        'Search "Air filter"',
+                        1000,
+                        'Search "Air Freshener"',
+                        1000,
+                        'Search "Batteries"',
+                        1000,
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style={{ fontSize: "15px", display: 'inline-block' }}
+                    repeat={Infinity}
+                    className="text-neutral-500 uppercase "
+                />
+            ) : (
+                <input type="text"
+                    defaultValue={searchText}
+                    onChange={handleOnChange}
+                    className='w-full h-full bg-transparent outline-none placeholder-gray-400 '
+                    placeholder="Search like egg, sugar and more.. " />
+
+            )}
+        </Link>
+    )
+}
+
+export default Search
